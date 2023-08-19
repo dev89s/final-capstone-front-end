cdfeae7366a8688c819a0123bf21fb747b90b5e4
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-19 12:12:08 +0300

    Open and close Mobile Menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,63 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './NavBar.css';
 import { Link, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
   const params = useLocation();
+  const [open, setOpen] = useState(true);
+
+  useEffect(() => {
+    setOpen(true);
+  }, [params]);
+
   return (
-    <ul className="nav">
-      <li className="logo">Logo</li>
-      <span className="links">
-        <li className={`${params.pathname === '/' && 'active'}`}>
-          <Link className="link" to="/">
-            Room
-          </Link>
-        </li>
-        <li
-          className={`${
-            params.pathname.split('/')[1] === 'reserve' && 'active'
-          }`}
+    <>
+      <div className="mobile">
+        <button
+          className="ham-menu"
+          type="button"
+          onClick={() => setOpen(!open)}
         >
-          <Link className="link" to="/reserve">
-            Reserve
-          </Link>
+          {open ? (
+            <svg viewBox="0 0 100 80" width="40" height="40" fill="black">
+              <rect width="100" height="10" />
+              <rect y="30" width="100" height="10" />
+              <rect y="60" width="100" height="10" />
+            </svg>
+          ) : (
+            <p>X</p>
+          )}
+        </button>
+      </div>
+      <ul className={`nav ${open && 'open'}`}>
+        <li className="logo">Logo</li>
+        <span className="links">
+          <li className={`${params.pathname === '/' && 'active'}`}>
+            <Link className="link" to="/">
+              Room
+            </Link>
+          </li>
+          <li
+            className={`${
+              params.pathname.split('/')[1] === 'reserve' && 'active'
+            }`}
+          >
+            <Link className="link" to="/reserve">
+              Reserve
+            </Link>
+          </li>
+          <li
+            className={`${
+              params.pathname.split('/')[1] === 'my_reservations' && 'active'
+            }`}
+          >
+            <Link className="link" to="/my_reservations">
+              My Reservations
+            </Link>
+          </li>
+          <li
+            className={`${
+              params.pathname.split('/')[1] === 'add_room' && 'active'
+            }`}
+          >
+            <Link className="link" to="/add_room">
+              Add Room
+            </Link>
+          </li>
+          <li
+            className={`${
+              params.pathname.split('/')[1] === 'delete_room' && 'active'
+            }`}
+          >
+            <Link className="link" to="/delete_room">
+              Delete Room
+            </Link>
+          </li>
+        </span>
+        <li>
+          <ul className="social">
+            <li>X</li>
+            <li>F</li>
+            <li>I</li>
+          </ul>
         </li>
-        <li
-          className={`${
-            params.pathname.split('/')[1] === 'my_reservations' && 'active'
-          }`}
-        >
-          <Link className="link" to="/my_reservations">
-            My Reservations
-          </Link>
-        </li>
-        <li
-          className={`${
-            params.pathname.split('/')[1] === 'add_room' && 'active'
-          }`}
-        >
-          <Link className="link" to="/add_room">
-            Add Room
-          </Link>
-        </li>
-        <li
-          className={`${
-            params.pathname.split('/')[1] === 'delete_room' && 'active'
-          }`}
-        >
-          <Link className="link" to="/delete_room">
-            Delete Room
-          </Link>
-        </li>
-      </span>
-      <li>
-        <ul className="social">
-          <li>X</li>
-          <li>F</li>
-          <li>I</li>
-        </ul>
-      </li>
-    </ul>
+      </ul>
+    </>
   );
 };
 
