df4fa77baed09e254a8e44d795db775ca5b632c5
ikoote1 <ikootepreim@gmail.com>
2023-08-19 10:51:42 +0300

    making some setups in app.js, index.js and home.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import NavBar from './components/NavBar';
 import Reserve from './components/Reserve';
 import MyReservations from './components/MyReservations';
 import DeleteRoom from './components/DeleteRoom';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="main">
-    <NavBar />
-    <div className="app-body">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/reserve" element={<Reserve />} />
-        <Route path="/my_reservations" element={<MyReservations />} />
-        <Route path="/add_room" element={<AddRoom />} />
-        <Route path="/delete_room" element={<DeleteRoom />} />
-      </Routes>
+      <NavBar />
+      <div className="app-body">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/reserve" element={<Reserve />} />
+          <Route path="/my_reservations" element={<MyReservations />} />
+          <Route path="/add_room" element={<AddRoom />} />
+          <Route path="/delete_room" element={<DeleteRoom />} />
+        </Routes>
+      </div>
     </div>
-  </div>
   );
 }
 
