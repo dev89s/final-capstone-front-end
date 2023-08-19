efb44a7f95f412cab12499d9a478898d51b86bfb
ikoote1 <ikootepreim@gmail.com>
2023-08-19 10:48:14 +0300

    Creating the Home page and making some setups

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import AddRoom from './components/AddRoom';
+import NavBar from './components/NavBar';
+import Reserve from './components/Reserve';
+import MyReservations from './components/MyReservations';
+import DeleteRoom from './components/DeleteRoom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    <NavBar />
+    <div className="app-body">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reserve" element={<Reserve />} />
+        <Route path="/my_reservations" element={<MyReservations />} />
+        <Route path="/add_room" element={<AddRoom />} />
+        <Route path="/delete_room" element={<DeleteRoom />} />
+      </Routes>
     </div>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from './app/store';
 
 import App from './App';
-import AddRoom from './components/AddRoom';
-import NavBar from './components/NavBar';
-import Reserve from './components/Reserve';
-import MyReservations from './components/MyReservations';
-import DeleteRoom from './components/DeleteRoom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <Provider store={store}>
     <BrowserRouter>
-      <div className="main">
-        <NavBar />
-        <div className="app-body">
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/reserve" element={<Reserve />} />
-            <Route path="/my_reservations" element={<MyReservations />} />
-            <Route path="/add_room" element={<AddRoom />} />
-            <Route path="/delete_room" element={<DeleteRoom />} />
-          </Routes>
-        </div>
-      </div>
+      <App />
     </BrowserRouter>
   </Provider>,
   // </React.StrictMode>,
