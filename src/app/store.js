4d1e2875917fcd256d59256149fdf65661a6bc9b
ikoote1 <ikootepreim@gmail.com>
2023-08-19 12:47:04 +0300

    adding reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import dataReducer from '../components/Home/reducer';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    data: dataReducer
+  },
 });
 
 export default store;
