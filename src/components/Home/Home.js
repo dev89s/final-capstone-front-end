deebd518d8cd827b84a4d4aec0355cec63928073
ikoote1 <ikootepreim@gmail.com>
2023-08-19 12:41:19 +0300

    fetching data in  home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import './Home.css';
+import { setData } from './actions'; // Create this action to set data in Redux
 
-const Home = () => (
-  <div className="homepage">
-    <h1>Home</h1>
-    <h1>Home</h1>
-  </div>
-);
+const Home = ({ data, setData }) => {
+  useEffect(() => {
+    axios.get('your_api_endpoint_here')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, [setData]);
 
-export default Home;
+  return (
+    <div className="homepage">
+      {data.map(item => (
+        <div className="item" key={item.id}>
+          <img src={item.imageUrl} alt={item.name} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  data: state.data // Assuming you have a reducer named 'data' in your Redux store
+});
+
+const mapDispatchToProps = {
+  setData // Action creator to set data in Redux
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
