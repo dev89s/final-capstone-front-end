d9108c699030878d6e26b0ca29d23e8eda9495ea
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-21 17:46:01 +0300

    Fix navigation display by adding location check

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import {
+  Route, Routes, useLocation, useNavigate,
+} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { setUsername } from './app/user/userSlice';
+import { setUserInfo } from './app/user/userSlice';
 import Login from './components/login/Login';
+import AddRoom from './components/AddRoom';
+import NavBar from './components/NavBar';
+import Reserve from './components/Reserve';
+import MyReservations from './components/MyReservations';
+import DeleteRoom from './components/DeleteRoom';
 
 function App() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const [loginPage, setLoginPage] = useState(false);
 
   const authenticateUser = () => {
     if (!localStorage.getItem('username')) {
       navigate('/login');
     } else {
-      dispatch(setUsername(localStorage.getItem('username')));
+      const user = JSON.parse(localStorage.getItem('username'));
+      dispatch(setUserInfo(user));
     }
   };
+
+  useEffect(() => {
+    if (location.pathname === '/login') {
+      setLoginPage(true);
+    } else {
+      setLoginPage(false);
+    }
+  }, [location]);
 
   useEffect(() => {
     authenticateUser();
@@ -23,9 +41,15 @@ function App() {
 
   return (
     <div className="main">
+      {!loginPage && <NavBar />}
       <div className="app-body">
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/" element={<div>home page </div>} />
+          <Route path="/reserve" element={<Reserve />} />
+          <Route path="/my_reservations" element={<MyReservations />} />
+          <Route path="/add_room" element={<AddRoom />} />
+          <Route path="/delete_room" element={<DeleteRoom />} />
         </Routes>
       </div>
     </div>
