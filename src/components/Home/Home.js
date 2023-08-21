18bad8a3f05c11d29314ab39033bfc397bcc4825
ikoote1 <ikootepreim@gmail.com>
2023-08-21 12:45:30 +0300

    changing from default redux to redux toolkit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,15 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'; // Import useDispatch and useSelector
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 import './Home.css';
-import { setData } from './actions';
+import { fetchRooms } from './dataSlice'; // Assuming you create a slice named dataSlice
 
 const Home = () => {
-  const dispatch = useDispatch(); // Get the dispatch function
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.dataSlice.data);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/v1/rooms')
-      .then((response) => {
-        console.log('data:', response.data);
-        dispatch(setData(response.data)); // Dispatch the action using the dispatch function
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
+    dispatch(fetchRooms()); // Dispatch the thunk action to fetch data
   }, [dispatch]);
-
-  const data = useSelector((state) => state.data); // Use useSelector to get data from the store
 
   return (
     <div className="homepage">
