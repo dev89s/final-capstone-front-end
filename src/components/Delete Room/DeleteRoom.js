935d2038ecbbce3ccf60d6f73b84385a72fba898
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-08-23 15:00:40 +0300

    Add description length check for description

--- src/components/Delete Room/DeleteRoom.js
+++ src/components/Delete Room/DeleteRoom.js
@@ -43,7 +43,7 @@ const DeleteRoom = () => {
               <h4 className="delete-room-name">{room.name}</h4>
               <p className="delete-room-description">
                 {room.description.split(' ').slice(0, 10).join(' ')}
-                {room.description.length > 70 ? ('...') : ''}
+                {room.description.split(' ').length > 10 ? ('...') : ''}
               </p>
               <button type="button" id={room.id} className="delete-btn" onClick={handleDelete}>
                 <img id={room.id} src={trashCan} alt="trash can" width={30} />
